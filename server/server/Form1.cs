41b4ac14d01353b7e18710492e412b17946aa6ed
Halenursozer <101124940+Halenursozer@users.noreply.github.com>
2022-03-23 00:26:19 +0300

    Update Form1.cs

--- server/server/Form1.cs
+++ server/server/Form1.cs
@@ -52,24 +52,6 @@ namespace server
             
         }
 
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
 
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
